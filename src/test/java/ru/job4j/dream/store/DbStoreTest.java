c10e1109ed0be7f27456dc3d0a2b84c2b2c2e865
dmitrykachala <kosas@mail.ru>
2022-01-11 12:55:28 +0300

    1. Написать тест на PostServlet. [#2518]

--- src/test/java/ru/job4j/dream/store/DbStoreTest.java
+++ src/test/java/ru/job4j/dream/store/DbStoreTest.java
@@ -1,16 +1,65 @@
 package ru.job4j.dream.store;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNull;
 
 public class DbStoreTest {
+
+    @Before
+    public void clear() {
+        BasicDataSource pool = new BasicDataSource();
+        Properties cfg = new Properties();
+        try (BufferedReader io = new BufferedReader(
+                new InputStreamReader(
+                        DbStore.class.getClassLoader()
+                                .getResourceAsStream("db.properties")
+                )
+        )) {
+            cfg.load(io);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        pool.setDriverClassName(cfg.getProperty("jdbc.driver"));
+        pool.setUrl(cfg.getProperty("jdbc.url"));
+        pool.setUsername(cfg.getProperty("jdbc.username"));
+        pool.setPassword(cfg.getProperty("jdbc.password"));
+        pool.setMinIdle(5);
+        pool.setMaxIdle(10);
+        pool.setMaxOpenPreparedStatements(100);
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement("DROP TABLE IF EXISTS post;\n"
+                     + "\n" + "CREATE TABLE IF NOT EXISTS post (\n" + "    id SERIAL PRIMARY KEY,\n"
+                     + "     name TEXT\n" + ");\n" + "\n" + "DROP TABLE IF EXISTS candidate;\n"
+                     + "\n" + "CREATE TABLE IF NOT EXISTS candidate (\n"
+                     + "    id SERIAL PRIMARY KEY,\n" + "    name TEXT\n" + ");")
+        ) {
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void whenFindAndCreateAndUpdateAndDeletePost() {
         Store store = DbStore.instOf();
